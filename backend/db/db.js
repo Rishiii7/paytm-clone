e20a56feeabe7fe1f6b5271946557faa59163a78
= <7rishi20ss@gmail.com>
2024-06-03 21:47:27 -0600

    Added Account schema and model

--- backend/db/db.js
+++ backend/db/db.js
@@ -13,9 +13,22 @@ const UserSchema = new mongoose.Schema({
     lastName: String,
 });
 
+const AccountSchema = new mongoose.Schema({
+    userId : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    }],
+    balance : {
+        type : Number,
+        required : true,
+    },
+});
+
 // Create a monogodb table
 const User = mongoose.model('User', UserSchema);
+const Account = mongoose.model("Account", AccountSchema);
 
 module.exports = {
     User,
+    Account,
 }
